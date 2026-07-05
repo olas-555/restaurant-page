c593d01aa2b635e113de24354d45695a9ac2796c
Isaac Abiodun <danielelina537@gmail.com>
2026-07-05 12:33:18 +0100

    Set up webpack project structure

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+
+    entry: "./src/index.js",
+
+    devtool: "inline-source-map",
+
+    devServer: {
+        static: "./dist",
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+};
